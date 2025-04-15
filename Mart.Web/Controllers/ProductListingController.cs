adfa165ee46fd73c51de40fbb226d31baeeeb699
gopivasanthi <gopi.sivanappanvasanthi@emea.nttdata.com>
2025-04-15 11:39:30 +0100

    Add Product View Modified

--- Mart.Web/Controllers/ProductListingController.cs
+++ Mart.Web/Controllers/ProductListingController.cs
@@ -1,6 +1,8 @@
 ﻿using Mart.Web.DbContext;
 using Mart.Web.Models;
+using Mart.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mart.Web.Controllers
@@ -15,10 +17,52 @@ namespace Mart.Web.Controllers
             return View(listOfProducts);
         }
         [HttpGet]
-        public IActionResult AddProduct()
+        public async Task<IActionResult> AddProduct()
         {
-            Product product = new();
-            return View(product);
+            ProductViewModel productViewModel = new();
+
+            var productCategories = await _dbContext.ProductCategories.ToListAsync();
+            //old way of doing it
+            List<SelectListItem> productCatSelectListItem = new List<SelectListItem>();
+            foreach(var prodcat in productCategories)
+            {
+                SelectListItem selectListItem = new SelectListItem
+                {
+                    Text = prodcat.CategoryName,
+                    Value = prodcat.CategoryId.ToString(),
+                };
+                productCatSelectListItem.Add(selectListItem);
+            }
+            //new way based on linq
+            var productCategorySelectListItems = productCategories.Select(prodcate => new SelectListItem
+            {
+                Text = prodcate.CategoryName,
+                Value = prodcate.CategoryId.ToString()
+            }).ToList();
+
+            productViewModel.ProductCategories = productCategorySelectListItems;
+
+            productViewModel.ProductColors = (await _dbContext.ProductColors.ToListAsync())
+                                        .Select(prodColor => new SelectListItem
+                                        {
+                                            Text = prodColor.ProductColorName,
+                                            Value = prodColor.ProductColorId.ToString()
+                                        });
+
+            productViewModel.ProductBrands = (await _dbContext.ProductBrands.ToListAsync())
+                                                    .Select(prodBrand => new SelectListItem
+                                                    {
+                                                        Text = prodBrand.ProductBrandName,
+                                                        Value = prodBrand.ProductBrandId.ToString()
+                                                    });
+            productViewModel.ProductAgeGroups = (await _dbContext.ProductAgeGroups.ToListAsync())
+                                                    .Select(prodAgeGroup => new SelectListItem
+                                                    {
+                                                        Text = prodAgeGroup.ProductAgeGroupName,
+                                                        Value = prodAgeGroup.ProductAgeGroupId.ToString()
+                                                    });
+
+            return View(productViewModel);
         }
         [HttpPost]
         public async Task<IActionResult> AddProduct(Product product)
